6fc2371099434224a14f55d941f90651146d4624
Paul DAO <dao.van.paul@gmail.com>
2022-02-08 15:00:22 -0500

    FEAT App routing : use auth guard on home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { AuthGuard } from './core/guards/auth.guard'
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./features/stacker/stacker.module').then((m) => m.StackerModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
   },
 ]
 
