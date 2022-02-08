88b893ae68d8e76eb8c1d01a53dad572361d843b
Paul DAO <dao.van.paul@gmail.com>
2022-02-08 14:59:18 -0500

    FEAT Auth : module + routing

---
+++ src/app/features/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { AuthService } from '../../shared/services/auth.service'
+
+import { AuthRoutingModule } from './auth-routing.module'
+import { LoginComponent } from './components/login/login.component'
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [CommonModule, AuthRoutingModule],
+  providers: [AuthService],
+  exports: [LoginComponent],
+})
+export class AuthModule {}
