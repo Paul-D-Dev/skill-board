2aed48928b2dafb753252bddbbc7555b62d3db38
Paul DAO <dao.van.paul@gmail.com>
2022-02-08 14:56:43 -0500

    FEAT Auth Service : sign in and sign out from AngularFireAuth

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core'
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { Router } from '@angular/router'
+import { GoogleAuthProvider } from 'firebase/auth'
+import { Observable, of, switchMap } from 'rxjs'
+import { IUser } from '../interfaces/user.interface'
+
+@Injectable()
+export class AuthService {
+  user$: Observable<IUser | null>
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          return of(user)
+        } else {
+          return of(null)
+        }
+      })
+    )
+  }
+
+  signInWithGoogle() {
+    const provider = new GoogleAuthProvider()
+    this.afAuth
+      .signInWithPopup(provider)
+      .then(() => {
+        console.log('Successfully logged in')
+        this.router.navigateByUrl('/')
+      })
+      .catch((error) => console.error(error))
+  }
+
+  async signOut() {
+    await this.afAuth.signOut()
+    return this.router.navigateByUrl('/')
+  }
+}
