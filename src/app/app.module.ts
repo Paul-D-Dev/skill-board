f6339c9e56fe00ef2d90c5b2e070fd620ff9100e
Paul DAO <dao.van.paul@gmail.com>
2022-02-08 14:59:39 -0500

    FEAT App module : import auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { AuthModule } from './features/auth/auth.module'
 import { AppFirebaseModule } from './shared/modules/app-firebase-module/app-firebase.module'
 
 @NgModule({
@@ -13,6 +14,7 @@ import { AppFirebaseModule } from './shared/modules/app-firebase-module/app-fire
     AppRoutingModule,
     HttpClientModule,
     AppFirebaseModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
