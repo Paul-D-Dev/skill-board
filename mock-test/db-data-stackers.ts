071ba7f422faaadb414253239dee2b699390e161
Paul DAO <dao.van.paul@gmail.com>
2022-02-14 16:43:42 -0500

    TEST : create mock stackers' response for testing

---
+++ mock-test/db-data-stackers.ts
@@ -0,0 +1,16 @@
+import { IStacker } from '../src/app/shared/interfaces/stacker.interface'
+
+export const STACKERS: IStacker[] = [
+  {
+    lastname: 'dao',
+    firstname: 'Paul',
+    skills: ['Angular', 'React', ' Vue'],
+    photoUrl: 'https://stack-labs.com/images/paul_300.jpg',
+  },
+  {
+    lastname: 'MORETTO',
+    firstname: 'Justin',
+    skills: ['Flutter'],
+    photoUrl: 'https://stack-labs.com/images/justin_300.jpg',
+  },
+]
