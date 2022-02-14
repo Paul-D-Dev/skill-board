bf2d77ed31e4a7bfff57fa819a3b017fa4937879
Paul DAO <dao.van.paul@gmail.com>
2022-02-14 16:44:05 -0500

    WIP TEST stacker component

--- src/app/features/stacker/components/stacker/stacker.component.spec.ts
+++ src/app/features/stacker/components/stacker/stacker.component.spec.ts
@@ -1,25 +1,70 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { of } from 'rxjs'
+import { STACKERS } from '../../../../../../mock-test/db-data-stackers'
+import { IStacker } from '../../../../shared/interfaces/stacker.interface'
+import { StackerService } from '../../../../shared/services/stacker.service'
+import { StackerModule } from '../../stacker.module'
 
-import { StackerComponent } from './stacker.component';
+import { StackerComponent } from './stacker.component'
 
-describe('StackerComponent', () => {
-  let component: StackerComponent;
-  let fixture: ComponentFixture<StackerComponent>;
+fdescribe('StackerComponent', () => {
+  let component: StackerComponent
+  let fixture: ComponentFixture<StackerComponent>
+  let stackerService: any
+  let el: DebugElement
+  const stackers = STACKERS
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ StackerComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      const stackerServiceSpy = jasmine.createSpyObj('StackerService', [
+        'getStackers',
+      ])
+      TestBed.configureTestingModule({
+        imports: [StackerModule],
+        declarations: [StackerComponent],
+        providers: [{ provide: StackerService, useValue: stackerServiceSpy }],
+      })
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(StackerComponent)
+          component = fixture.componentInstance
+          el = fixture.debugElement
+          stackerService = TestBed.inject(StackerService)
+        })
     })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StackerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  )
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should return a collection of stackers', () => {
+    component.stackersList = []
+    stackerService.getStackers.and.returnValue(of(stackers))
+    stackerService.getStackers().subscribe((res: IStacker[]) => {
+      component.stackersList = res
+    })
+
+    expect(component.stackersList).toEqual(
+      stackers,
+      'stackersList is not as expected'
+    )
+  })
+
+  it('should display stackers cards', () => {
+    stackerService.getStackers.and.returnValue(of(stackers))
+    fixture.detectChanges()
+    const stackersCards = el.queryAll(By.css('.stackers__card'))
+    expect(stackersCards.length).toBeGreaterThan(
+      0,
+      'Could not find stackers cards'
+    )
+
+    expect(stackersCards[0].nativeElement.textContent).toContain(
+      'DAO',
+      'Not same lastname'
+    )
+  })
+})
