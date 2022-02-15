017d1819e6080818dc2f00da26d4d7c01b3a0068
Paul DAO <dao.van.paul@gmail.com>
2022-02-15 15:17:43 -0500

    TEST Skill Tag: Unit Test + E2E

--- src/app/shared/components/skill-tag/skill-tag.component.spec.ts
+++ src/app/shared/components/skill-tag/skill-tag.component.spec.ts
@@ -1,25 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { STACKERS } from '../../../../../mock-test/db-data-stackers.mock'
 
-import { SkillTagComponent } from './skill-tag.component';
+import { SkillTagComponent } from './skill-tag.component'
 
-describe('SkillTagComponent', () => {
-  let component: SkillTagComponent;
-  let fixture: ComponentFixture<SkillTagComponent>;
+fdescribe('SkillTagComponent', () => {
+  let component: SkillTagComponent
+  let fixture: ComponentFixture<SkillTagComponent>
+  const stackers = STACKERS
+  let el: DebugElement
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SkillTagComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SkillTagComponent],
+      })
     })
-    .compileComponents();
-  });
+  )
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SkillTagComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(SkillTagComponent)
+    component = fixture.componentInstance
+    el = fixture.debugElement
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should display data input', () => {
+    component.skillsProps = stackers[0].skills
+    fixture.detectChanges()
+
+    const skillsTag = el.queryAll(By.css('.skill-tag'))
+    expect(component.skillsProps.length).toEqual(
+      skillsTag.length,
+      'All skill do not display'
+    )
+    for (let i = 0; i < component.skillsProps.length; i++) {
+      const nameSkill = skillsTag[i].nativeElement.innerText.trim()
+      expect(component.skillsProps[i].trim()).toContain(
+        nameSkill,
+        'skill name is different'
+      )
+    }
+  })
+})
