90006f28dea45f1d1c1c7d48ac164db776e5af5f
Paul DAO <dao.van.paul@gmail.com>
2022-02-15 15:17:23 -0500

    CHORES Skill Tag Component : remove ngTagsOnMobile

--- src/app/shared/components/skill-tag/skill-tag.component.ts
+++ src/app/shared/components/skill-tag/skill-tag.component.ts
@@ -1,24 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-skill-tag',
   templateUrl: './skill-tag.component.html',
   styleUrls: ['./skill-tag.component.scss'],
 })
-export class SkillTagComponent implements OnInit {
+export class SkillTagComponent {
   @Input('skills') skillsProps: string[] = ['']
 
   constructor() {}
-
-  ngOnInit() {
-    const widthPage = window.innerWidth
-    if (widthPage < 640 && this.skillsProps.length > 1) {
-      this.skillsProps = this.nbTagsOnMobile(this.skillsProps)
-    }
-  }
-
-  nbTagsOnMobile(tags: string[]): string[] {
-    const nbRestTags = tags.length - 1
-    return [tags[0], '+ ' + nbRestTags.toString()]
-  }
 }
