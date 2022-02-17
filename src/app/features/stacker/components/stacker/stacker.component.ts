d927609918b1ab30a8634b5bdbccaffd86b2e9fd
Paul DAO <dao.van.paul@gmail.com>
2022-02-17 12:33:40 -0500

    FEAT Stacker component : add skills filter

--- src/app/features/stacker/components/stacker/stacker.component.ts
+++ src/app/features/stacker/components/stacker/stacker.component.ts
@@ -8,7 +8,10 @@ import { StackerService } from '../../../../shared/services/stacker.service'
   styleUrls: ['./stacker.component.scss'],
 })
 export class StackerComponent implements OnInit {
+  initialStateStackers: IStacker[] = []
   stackersList: IStacker[] = []
+  filterListSkills: string[] = []
+  skillToFilter: string[] = []
   constructor(private stackerService: StackerService) {}
 
   ngOnInit(): void {
@@ -18,7 +21,48 @@ export class StackerComponent implements OnInit {
   getStacker() {
     this.stackerService.getStackers().subscribe((stackers) => {
       console.log(stackers)
-      this.stackersList = stackers
+      this.initialStateStackers = this.stackersList = stackers
+      this.setFilterSkillList(this.stackersList)
     })
+  }
+
+  setFilterSkillList(stackers: IStacker[]): void {
+    // For each stacker push their skills
+    for (const stacker of stackers) {
+      this.filterListSkills.push(...stacker.skills)
+    }
+
+    // Remove duplicate
+    this.filterListSkills = [...new Set(this.filterListSkills)]
+  }
+
+  setFilterSkills(skill: string): void {
+    // Remove skill
+    if (this.skillToFilter.includes(skill)) {
+      const indexSkill = this.skillToFilter.indexOf(skill)
+      this.skillToFilter.splice(indexSkill, 1)
+    }
+    // Add skill
+    else {
+      this.skillToFilter.push(skill)
+    }
+
+    this.filterSkills(this.skillToFilter)
+  }
+
+  filterSkills(skillsToFilter: string[]): void {
+    this.stackersList = this.initialStateStackers.filter((stacker) => {
+      return skillsToFilter.every((skill) => stacker.skills.includes(skill))
+    })
+
+    if (this.skillToFilter.length === 0) {
+      this.stackersList = this.initialStateStackers
+    }
+  }
+
+  resetFilter(): void {
+    this.skillToFilter = []
+    // Uncheck selected checkbox
+    this.stackersList = this.initialStateStackers
   }
 }
