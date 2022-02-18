ff431a10afc5ee7540ea410000f01f4497d86617
Paul DAO <dao.van.paul@gmail.com>
2022-02-18 12:22:48 -0500

    WIP STYLE stacker filter

--- src/app/features/stacker/stacker.module.ts
+++ src/app/features/stacker/stacker.module.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { SkillTagModule } from '../../shared/components/skill-tag/skill-tag.module'
 import { StackerService } from '../../shared/services/stacker.service'
+import { NavbarModule } from '../navbar/navbar.module'
 import { StackerComponent } from './components/stacker/stacker.component'
 
 import { StackerRoutingModule } from './stacker-routing.module'
 
 @NgModule({
   declarations: [StackerComponent],
-  imports: [CommonModule, StackerRoutingModule, SkillTagModule],
+  imports: [CommonModule, StackerRoutingModule, SkillTagModule, NavbarModule],
   providers: [StackerService],
 })
 export class StackerModule {}
